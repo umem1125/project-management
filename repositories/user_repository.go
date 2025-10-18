bffa3f4089dbe5b6058236889cb377939ea0805d
umem1125 <chaerulumam1125@gmail.com>
2025-10-18 11:19:37 +0700

    create user repository and service

---
+++ repositories/user_repository.go
@@ -0,0 +1,29 @@
+package repositories
+
+import (
+	"github.com/umem1125/project-management/config"
+	"github.com/umem1125/project-management/models"
+)
+
+type UserRepository interface {
+	Create(user *models.User) error
+	FindByEmail(email string) (*models.User, error)
+}
+
+type userRepository struct {
+
+}
+
+func NewUserRepository() UserRepository {
+	return &userRepository{}
+}
+
+func (r *userRepository) Create (user *models.User) error {
+	return config.DB.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string)(*models.User, error) {
+	var user models.User
+	err := config.DB.Where("email =  ?",  email).First(&user).Error
+	return &user,err
+}
---
+++ services/user_service.go
@@ -0,0 +1,44 @@
+// Bussines Logic Here
+package services
+
+import (
+	"errors"
+
+	"github.com/google/uuid"
+	"github.com/umem1125/project-management/models"
+	"github.com/umem1125/project-management/repositories"
+	"github.com/umem1125/project-management/utils"
+)
+
+type UserService interface {
+	Register(user *models.User) error
+}
+
+type userService struct {
+	repo repositories.UserRepository
+}
+
+func NewUserService(repo repositories.UserRepository) UserService {
+	return &userService{}
+}
+
+func (s *userService)Register(user *models.User) error {
+	// cek email sudah tercaftar atau belum
+	// hashing password
+	// set role
+	// simpan user
+
+	existingUser , _ := s.repo.FindByEmail(user.Email)
+	if existingUser.InternalID != 0 {
+		return errors.New("email already registered")
+	}
+	hashed, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+
+	user.Password = hashed
+	user.Role = "user"
+	user.PublicID = uuid.New()
+	return s.repo.Create(user)
+}
